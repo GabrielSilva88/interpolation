798e5374b2e2a7844dc44293e97314fe631c1c0d
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-01-05 10:25:49 -0300

    Aula interpolação de animação 45.USANDO INTERPOLAÇÃO UDEMY

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      larAnimada: new Animated.Value(0)
+    };
+
+    Animated.timing(
+      this.state.larAnimada,
+      {
+        toValue: 100,
+        duration: 7000
+      }
+    ).start();
+  }
+
+  render() {
+
+    let porcentagem = this.state.larAnimada.interpolate({
+      inputRange: [0, 100],
+      outputRange: ['0%', '100%']
+    });
+
+    return (
+      <View style={styles.container}>
+
+        <Animated.View style={{
+          backgroundColor: 'blue',
+          width: porcentagem,
+          height: 25
+        }}>
+
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
 });
